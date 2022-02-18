d008c0a7f6de456e23afe002ed16a806587c1b86
G.F.Jankavs <89642177+GFJankavs@users.noreply.github.com>
2022-02-18 16:01:41 +0200

    update About
    commiting my work done on About Us page so far

---
+++ src/data/processData.ts
@@ -0,0 +1,25 @@
+const processData = [
+  {
+    title: "Planning",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+  },
+  {
+    title: "Conception",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+  },
+  {
+    title: "Design",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+  },
+  {
+    title: "Development",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+  },
+];
+
+export default processData;
+
+export interface ProcessDataType {
+  title: string;
+  description: string;
+}
